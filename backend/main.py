2c31bfca47958698fde58f7ad045ef3259660480
Maria Fernanda Tolosa <fernandaangel242@gmail.com>
2026-04-20 11:16:44 -0500

    fix imports backend definitivos

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from database import engine, Base
-from routes import usuarios, rutinas, progreso
+from backend.database import engine, Base
+from backend.routes import usuarios, rutinas, progreso
 
 Base.metadata.create_all(bind=engine)
 
